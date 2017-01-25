5fc7b975f3fb295f18fa1b155922228473b4e460
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-25 21:58:01 +0800

    add github resource

---
+++ src/main/java/com/stayrascal/services/rest/GithubResource.java
@@ -0,0 +1,26 @@
+package com.stayrascal.services.rest;
+
+import com.stayrascal.services.model.GithubUser;
+import com.stayrascal.services.service.GithubService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/github/users")
+public class GithubResource {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private GithubService githubService;
+
+    @RequestMapping("/{username}")
+    public GithubUser getUser(@PathVariable("username") String username) {
+        logger.info("Get git.com/{} info", login);
+        return githubService.getUser(username);
+    }
+}
