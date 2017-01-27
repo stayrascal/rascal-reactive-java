adee1f0400be49ea52957ae7ba07e2b21face2bd
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-27 21:42:24 +0800

    add response data model

--- src/main/java/com/stayrascal/services/util/rest/dto/jsonapi/RespopnseData.java
+++ src/main/java/com/stayrascal/services/util/rest/dto/jsonapi/RespopnseData.java
@@ -1,9 +1,44 @@
 package com.stayrascal.services.util.rest.dto.jsonapi;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.io.Serializable;
 
-@JsonPropertyOrder({"type"})
-public class RespopnseData< T extends Object> implements Serializable {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"type", "id", "attributes"})
+public class RespopnseData<T extends Object> implements Serializable {
+    private String type;
+    private String id;
+    private T attributes;
+
+    public RespopnseData(String type, String id, T attributes) {
+        this.type = type;
+        this.id = id;
+        this.attributes = attributes;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public T getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(T attributes) {
+        this.attributes = attributes;
+    }
 }
