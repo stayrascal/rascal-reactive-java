26537f275ab558e3e90c79666af18c16da01d697
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-12 16:10:57 +0800

    add guava dependency

--- src/main/java/com/stayrascal/services/v1/domain/model/CustomerDto.java
+++ src/main/java/com/stayrascal/services/v1/domain/model/CustomerDto.java
@@ -1,5 +1,9 @@
 package com.stayrascal.services.v1.domain.model;
 
+import com.google.common.collect.Lists;
+import com.stayrascal.services.model.GithubRepository;
+import com.stayrascal.services.model.GithubUser;
+import com.stayrascal.services.model.RawUser;
 import com.stayrascal.services.v1.domain.Customer;
 import com.stayrascal.services.v1.domain.Product;
 import com.stayrascal.services.v1.domain.address.Address;
@@ -7,11 +11,13 @@ import com.stayrascal.services.v1.domain.address.Address;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 public class CustomerDto {
     private Customer customer;
     private List<Product> products = new ArrayList<>();
     private List<Address> addresses = new LinkedList<>();
+    private Optional<GithubUser> githubUser;
 
     public Customer getCustomer() {
         return customer;
@@ -25,7 +31,7 @@ public class CustomerDto {
         products.add(product);
     }
 
-    public void addProducts(List<Product> products){
+    public void addProducts(List<Product> products) {
         products.addAll(products);
     }
 
@@ -33,7 +39,7 @@ public class CustomerDto {
         addresses.add(address);
     }
 
-    public void addAddresses(List<Address> addresses){
+    public void addAddresses(List<Address> addresses) {
         addresses.addAll(addresses);
     }
 
@@ -47,5 +53,21 @@ public class CustomerDto {
 
     public List<Address> getAddresses() {
         return addresses;
+    }
+
+    public Optional<GithubUser> getGithubUser() {
+        return githubUser;
+    }
+
+    public List<RawUser> getGithubFollowers() {
+        return githubUser
+                .map(user -> user.getFollowers())
+                .orElse(Lists.newArrayList());
+    }
+
+    public List<GithubRepository> getGithRepository() {
+        return githubUser
+                .map(user -> user.getRepositories())
+                .orElse(Lists.newArrayList());
     }
 }
