e3f0673b08b7d0456a458402dc6f4e7da990e41d
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-12 14:29:37 +0800

    update audit model

--- src/main/java/com/stayrascal/services/v1/model/address/City.java
+++ src/main/java/com/stayrascal/services/v1/model/address/City.java
@@ -1,17 +1,37 @@
 package com.stayrascal.services.v1.model.address;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RASCAL_CITY", indexes = @Index(columnList = "CITY_ID"))
 public class City {
 
+    @Id
+    @GeneratedValue(generator = "generator", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "generator", sequenceName = "SEQ_RASCAL_CITY", allocationSize = 1)
+    @Column(name = "CITY_ID")
     private Long cityId;
 
+    @Column(name = "CITY_NAME", nullable = false, length = 10)
     private String cityName;
 
+    @ManyToOne
+    @JoinColumn(name = "PROVINCE_ID", nullable = false)
     private Province province;
 
-    private List<City> oldNames = new ArrayList<>();
 
+    @Type(type = "yes_no")
+    @Column(name = "STATUS", nullable = false, length = 1)
     private boolean status;
 }
---
+++ src/main/java/com/stayrascal/services/v1/model/audit/Auditable.java
@@ -0,0 +1,69 @@
+package com.stayrascal.services.v1.model.audit;
+
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public class Auditable {
+
+    @CreatedDate
+    @Column(name = "CREATED_DATETIME")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_DATETIME")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+    private LocalDateTime updatedDateTime;
+
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "UPDATED_BY")
+    private String updatedBy;
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public LocalDateTime getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    public void setUpdatedDateTime(LocalDateTime updatedDateTime) {
+        this.updatedDateTime = updatedDateTime;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
