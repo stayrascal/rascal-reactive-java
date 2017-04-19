dcc4ffe7f1816acbafd82bbba9825dc61e26e779
Zhiping Wu <zpwu@thoughtworks.com>
2017-04-19 22:59:22 +0800

    add moco

---
+++ src/test/intg/java/com/stayrascal/services/MocoIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.stayrascal.services;
+
+import com.github.dreamhead.moco.HttpServer;
+import com.github.dreamhead.moco.Runner;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.springframework.core.io.ClassPathResource;
+
+import static com.github.dreamhead.moco.Moco.httpServer;
+import static com.github.dreamhead.moco.Moco.pathResource;
+
+public class MocoIntegrationTest extends ReactiveApplicationTest {
+    private static Runner runner;
+
+    @BeforeClass
+    public static void startUp() throws Exception {
+        HttpServer server = httpServer(8882);
+        server.response(pathResource(
+                new ClassPathResource("api/jason/moco-response.json").getFile().getAbsolutePath()));
+        runner = Runner.runner(server);
+        runner.start();
+    }
+
+    @AfterClass
+    public static void shutDown() throws Exception {
+        runner.stop();
+    }
+
+}
+
