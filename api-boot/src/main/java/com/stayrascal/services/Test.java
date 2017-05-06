d4e4a4d6a42a0ad10858890874408a0574ceab28
Zhiping Wu <zpwu@thoughtworks.com>
2017-05-06 21:34:57 +0800

    update api util

--- api-boot/src/main/java/com/stayrascal/services/Test.java
+++ api-boot/src/main/java/com/stayrascal/services/Test.java
@@ -6,9 +6,11 @@ public class Test {
     public static void main(String[] args) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(6);
         String hashedPassword = passwordEncoder.encode("password");
+        System.out.println(passwordEncoder.matches("password", "$2a$06$dl1J.PeDVnliIU6bFGR8kuSShAVaylnaieZ872Em2be7ADUcTeUEG"));
+        System.out.println(passwordEncoder.matches("password", "$2a$06$FxYloy7rsuuInOsthcjT5uthklfuVqUvOQoc172JXGJK3uEQnDl82"));
         System.out.println(hashedPassword);
-        System.out.println(passwordEncoder.encode("apiuser:password"));
-        System.out.println(passwordEncoder.encode("apiuser:password"));
+        System.out.println(passwordEncoder.encode("password"));
+        System.out.println(passwordEncoder.encode("password"));
         System.out.println(passwordEncoder.encode("password"));
     }
 }
