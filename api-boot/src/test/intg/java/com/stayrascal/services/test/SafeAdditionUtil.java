02946e9ea682e0a3a8fea0c5426bda0dceacd891
Zhiping Wu <zpwu@thoughtworks.com>
2017-06-06 20:33:26 +0800

    add params test case and demo

---
+++ api-boot/src/test/intg/java/com/stayrascal/services/test/SafeAdditionUtil.java
@@ -0,0 +1,13 @@
+package com.stayrascal.services.test;
+
+public class SafeAdditionUtil {
+    public int safeAdd(int a, int b) {
+        long result = ((long) a) + b;
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } else if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int) result;
+    }
+}
